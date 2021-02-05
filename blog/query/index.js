67883d6cae87db4d34d26ce3d590c1bc762276da
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2021-02-05 11:32:56 +0530

    Added query service

---
+++ blog/query/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const posts = {};
+
+app.get("/posts", (req, res) => {
+  res.json(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "PostCreated") {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === "CommentCreated") {
+    const { id, content, postID } = data;
+    posts[postID]["comments"].push({ id, content });
+  }
+
+  console.log(posts);
+  res.json({});
+});
+
+app.listen(4002, () => console.log("query service listening on 4002..."));
