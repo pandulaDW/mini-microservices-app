39516ed2ea675552d1c3555993e0661d27320b6c
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2021-02-05 11:02:48 +0530

    Added event emission from services

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 const { randomBytes } = require("crypto");
 
 const app = express();
@@ -14,7 +15,7 @@ app.get("/posts/:id/comments", (req, res) => {
   res.json(commentsByPostID[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const commentID = randomBytes(4).toString("hex");
   const postID = req.params.id;
   const { content } = req.body;
@@ -25,7 +26,16 @@ app.post("/posts/:id/comments", (req, res) => {
 
   const newComment = { id: commentID, content };
   commentsByPostID[postID].push(newComment);
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: { ...newComment, postID },
+  });
   res.status(201).json(newComment);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Received event", req.body);
+  res.send("");
 });
 
 app.listen(4001, () => {
--- blog/posts/index.js
+++ blog/posts/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 const { randomBytes } = require("crypto");
 
 const app = express();
@@ -14,12 +15,25 @@ app.get("/posts", (_, res) => {
   res.json(posts);
 });
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
   posts[id] = { id, title };
 
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: {
+      id,
+      title,
+    },
+  });
+
   res.status(201).json(posts[id]);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Received event", req.body);
+  res.send("");
 });
 
 app.listen(4000, () => {
