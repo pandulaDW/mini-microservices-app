b2d052ba091f4b23512205a85ff7c080e4702117
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2021-02-07 11:07:11 +0530

    Rendering comments based on status

--- blog/client/src/components/CommentList.tsx
+++ blog/client/src/components/CommentList.tsx
@@ -3,17 +3,25 @@ import React from "react";
 interface Comment {
   id: string;
   content: string;
+  status: "approved" | "pending" | "rejected";
 }
 
 interface Props {
   comments: Comment[];
 }
 
+const renderContent = (comment: Comment): string => {
+  let content = comment.content;
+  if (comment.status === "pending") content = "Awaiting moderation";
+  if (comment.status === "rejected") content = "Rejected!!!";
+  return content;
+};
+
 const CommentList: React.FC<Props> = ({ comments }) => {
   return (
     <ul>
       {comments.map((comment) => (
-        <li key={comment.id}>{comment.content}</li>
+        <li key={comment.id}>{renderContent(comment)}</li>
       ))}
     </ul>
   );
